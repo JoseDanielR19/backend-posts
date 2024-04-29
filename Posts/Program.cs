faf7d7301ae11efeac306398e9a76887735460a4
Jose Daniel Rodriguez Bedoya <jrodriguezb@SUMMIT.COM.CO>
2024-04-29 11:13:34 -0500

    Changed the Program class

--- Posts/Program.cs
+++ Posts/Program.cs
@@ -1,13 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.WithOrigins("http://localhost:4200", "http://localhost:4200/*").AllowAnyHeader().AllowAnyHeader()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +16,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
+app.UseCors(options => options.WithOrigins("http://localhost:4200", "http://localhost:4200/*").AllowAnyHeader().AllowAnyMethod().WithExposedHeaders());
 
 app.UseHttpsRedirection();
 
